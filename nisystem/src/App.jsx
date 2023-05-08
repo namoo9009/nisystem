f3af1cdaedf9eb160ce7bd5634aef02e23faaee0
namo9009 <namoo9009@naver.com>
2023-05-08 16:19:44 +0900

    명령어 추가

--- nisystem/src/App.jsx
+++ nisystem/src/App.jsx
@@ -6,7 +6,7 @@ import "./styles/reset.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/company" element={<Home />} />
